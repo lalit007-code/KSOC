330f0ed77b4d90344c898f907a3b038e5ba9358e
Lalit Singh <lalit284546@gmail.com>
2024-09-08 13:06:29 +0530

    just added id on nvabar list map

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,7 @@
-"use client"
-import Image from 'next/image';
-import { useRouter } from 'next/router'
-import React, { useEffect, useState } from 'react'
+"use client";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
 
 export default function Navbar() {
   const [mounted, setMounted] = useState(false);
@@ -16,50 +16,72 @@ export default function Navbar() {
 
   // const router = useRouter();
 
-  const currentPath = '/about';
+  const currentPath = "/about";
   const navList = [
     {
-      path: '/',
-      text: 'Home',
-      icon: '',
+      id: 1,
+      path: "/",
+      text: "Home",
+      icon: "",
+    },
+
+    {
+      id: 2,
+      path: "/about",
+      text: "About",
+      icon: "",
     },
     {
-      path: '/about',
-      text: 'About',
-      icon: '',
+      id: 3,
+      path: "/services",
+      text: "Services",
+      icon: "",
     },
     {
-      path: '/services',
-      text: 'Services',
-      icon: '',
+      id: 4,
+      path: "/testimonials",
+      text: "Testimonials",
+      icon: "",
     },
     {
-      path: '/testimonials',
-      text: 'Testimonials',
-      icon: '',
+      id: 5,
+      path: "/contact",
+      text: "Contact",
+      icon: "",
     },
     {
-      path: '/contact',
-      text: 'Contact',
-      icon: '',
+      id: 6,
+      path: "/team",
+      text: "Team",
+      icon: "",
     },
-    {
-      path: '/team',
-      text: 'Team',
-      icon: '',
-    },
-  ]
+  ];
 
   return (
-    <nav className='flex justify-between items-center px-8 py-4 bg-transparent text-white font-semibold '>
-      <span className=''>
-        <Image src='/images/logo.png' className="" height={300} width={300} alt="logo"/>
+    <nav className="flex justify-between items-center px-8 py-4 bg-transparent text-white font-semibold ">
+      <span className="">
+        <Image
+          src="/images/logo.png"
+          className=""
+          height={300}
+          width={300}
+          alt="logo"
+        />
       </span>
-      <ul className='flex gap-4 text-lg'>
+      <ul className="flex gap-4 text-lg">
         {navList.map((item) => (
-          <li className={currentPath===item.path?'text-blue-500 cursor-pointer font-semibold':'hover:text-blue-500 cursor-pointer font-semibold'}>{item?.text}</li>
+          <li
+            key={item.id}
+            className={
+              currentPath === item.path
+                ? "text-blue-500 cursor-pointer font-semibold"
+                : "hover:text-blue-500 cursor-pointer font-semibold"
+            }
+          >
+            {item?.text}
+          </li>
         ))}
       </ul>
     </nav>
-  )
+  );
 }
