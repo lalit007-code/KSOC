26fd5fd72182051ff2f7baaf4ab362fe7a77748e
Lalit Singh <lalit284546@gmail.com>
2024-09-09 12:48:56 +0530

    init resend for email verifcation

---
+++ helpers/sendVerification.ts
@@ -0,0 +1,4 @@
+import { Resend } from "@/lib/resend";
+import VerificationEmail from "@/emails/VerificationEmail";
+
+
--- lib/db.ts
+++ lib/db.ts
@@ -1,67 +1,24 @@
-import mongoose, { Document, Model, Schema } from "mongoose";
+import mongoose from "mongoose";
 
-// Define the interface for the User document
-export interface IUser extends Document {
-  name: string;
-  email: string;
-
-  password: string;
-  verifiyCode: string;
-  verifyCodeExpiry: Date;
-
-  createdAt?: Date;
-  updatedAt?: Date;
-}
-
-export interface IUserDetails extends Document {
-  phone: string;
-  class: string;
-  fatherName: string;
-  aboutUser?: string;
-  createdAt?: Date;
-  updatedAt?: Date;
-}
-
-// Define the Mongoose schema
-const userDetails: Schema<IUserDetails> = new Schema(
-  {
-    phone: { type: String, required: true },
-    class: { type: String, required: [true, "class is required"] },
-    fatherName: {
-      type: String,
-      required: [true, "fatherName is required"],
-      trim: true,
-    },
-    aboutUser: { type: String },
-  },
-  { timestamps: true }
-);
-
-const userSchema: Schema<IUser> = new Schema(
-  {
-    name: { type: String, required: [true, "name is required"], trim: true },
-    email: {
-      type: String,
-      required: [true, "email is required"],
-      unique: true,
-      match: [/.+\@.+\..+/, "email is invalid"],
-    },
-    password: { type: String, required: true },
-    verifiyCode: { type: String, required: true },
-    verifyCodeExpiry: { type: Date, required: true },
-  },
-  { timestamps: true }
-);
-
-// Create the Mongoose model
-const UserModel: Model<IUser> =
-  mongoose.models.User || mongoose.model<IUser>("User", userSchema);
-
-const UserDetailsModel: Model<IUserDetails> =
-  mongoose.models.UserDetails ||
-  mongoose.model<IUserDetails>("UserDetails", userDetails);
-
-export default {
-  UserModel,
-  UserDetailsModel,
+type ConnectionObject = {
+  isConnected?: number;
 };
+
+const connection: ConnectionObject = {};
+
+async function dbConnect(): Promise<void> {
+  if (connection.isConnected) {
+    console.log("already connected to database");
+  }
+  try {
+    const db = await mongoose.connect(process.env.MONGODB_URL || "", {});
+    connection.isConnected = db.connections[0].readyState;
+    console.log("connected to database");
+  } catch (err) {
+    console.log("Database connection error", err);
+
+    process.exit(1);
+  }
+}
+
+export default dbConnect;
